a0d888e44907ab8152ebc3de8760d8fa52b522dd
KaizerFox <29178963+KaizerFox@users.noreply.github.com>
2020-06-04 18:22:57 -0400

    Update grabscript.lua

--- grabscript.lua
+++ grabscript.lua
@@ -9,8 +9,11 @@ local IgnoreCharacter = true
 local MaxParts = 20
 local Disable_Collision = false
 local Range = 4
+local AnchorSound = "rbxasset://sounds/electronicpingshort.wav"
+local UnanchorSound = "rbxasset://sounds/electronicpingshort.wav" -- "rbxasset://sounds/swoosh.wav"
 --Script
 local TargetBricks = {}
+local AnchorTargets = {}
 local TargetPos
 local TargetRot = CFrame.new(0,0,0)
 local Distance = 0
@@ -21,6 +24,8 @@ local CurRot = Vector3.new(0,0,0)
 
 local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
 local UIS = game:GetService("UserInputService")
+
+local Anchors = {}
 
 function Lerp(PointA,PointB,Alpha)
 	return PointA * (1-Alpha) + PointB * Alpha
@@ -34,6 +39,14 @@ function GetCFrameAngles(CF) --GetCFrameAngles(CFrame)
 	return Response
 end
 
+function GetLength(Table)
+	local Length = 0
+	for k,v in pairs(Table) do
+		Length = Length + 1
+	end
+	return Length
+end
+
 function UpdateVelocity(FrameTime)
 	for i,TargetBrick in pairs(TargetBricks) do
 		if (not TargetBrick) or (TargetBrick and not TargetPos) then
@@ -41,7 +54,19 @@ function UpdateVelocity(FrameTime)
 		end
 		TargetBrick.Velocity = (TargetPos - TargetBrick.CFrame.Position) * 60
 		
+		TargetBrick.RotVelocity = Vector3.new(0,0,0)
+		
 		TargetBrick.CFrame = TargetBrick.CFrame:Lerp(TargetRot - TargetRot.Position + TargetPos,FrameTime*16)
+	end
+	for i,TargetData in pairs(Anchors) do
+		if (not TargetData[1]) then
+			return
+		end
+		TargetData[1].Velocity = (TargetData[2].Position - TargetData[1].Position) * 60
+		
+		TargetData[1].RotVelocity = Vector3.new(0,0,0)
+		
+		TargetData[1].CFrame = TargetData[1].CFrame:Lerp(TargetData[2] - TargetData[2].Position + TargetData[2].Position,FrameTime*16)
 	end
 end
 
@@ -74,21 +99,21 @@ function UpdateVisual(FrameTime)
 end
 
 function HandleBrick(TargetBrick)
-	local roblox_is_fucking_retarded = false
+	local passed = false
 	if TargetBrick == nil then
-		roblox_is_fucking_retarded = true
+		passed = true
 	else
 		if not TargetBrick:IsA("BasePart") then
-			roblox_is_fucking_retarded = true
+			passed = true
 		end
-		if roblox_is_fucking_retarded or TargetBrick:IsA("Terrain") then
-			roblox_is_fucking_retarded = true
+		if passed or TargetBrick:IsA("Terrain") then
+			passed = true
 		end
-		if roblox_is_fucking_retarded or TargetBrick.Anchored then
-			roblox_is_fucking_retarded = true
+		if passed or TargetBrick.Anchored then
+			passed = true
 		end
 	end
-	if not roblox_is_fucking_retarded then
+	if not passed then
 		Transparencys[TargetBrick] = TargetBrick.Transparency
 		CanCollides[TargetBrick] = TargetBrick.CanCollide
 		Distance = (workspace.CurrentCamera.CFrame.Position-TargetBrick.Position).Magnitude
@@ -110,10 +135,12 @@ Mouse.Button1Down:Connect(function()
 		else
 			Parts = workspace:FindPartsInRegion3(Region,nil,MaxParts)
 		end
+		AnchorTargets = {}
 		for i,Part in pairs(Parts) do
 			local Result = HandleBrick(Part)
 			if Result then
 				table.insert(TargetBricks,#TargetBricks+1,Result)
+				table.insert(AnchorTargets,#AnchorTargets+1,Result)
 				TargetRot = Result.CFrame
 			end
 		end
@@ -179,6 +206,50 @@ UIS.InputEnded:Connect(function(InputObject)
 	if InputObject.KeyCode == Enum.KeyCode.H then
 		--RotRight
 		CurRot = CurRot + Vector3.new(0,0,1)
+	end
+	if InputObject.KeyCode == Enum.KeyCode.J then
+		--Anchor
+		if GetLength(Anchors) > 0 then
+			local did = false
+			for i,Anchor in pairs(Anchors) do
+				if Anchor[1] then
+					Anchor[1].CFrame = Anchor[2]
+					Anchor[1].Color = Anchor[3]
+					Anchor[1].Velocity = Anchor[4]
+					Anchor[1].RotVelocity = Anchor[5]
+					did = true
+				end
+			end
+			Anchors = {}
+			if did then
+				local sfx = Instance.new("Sound",game.SoundService)
+				sfx.Volume = 1
+				sfx.SoundId = UnanchorSound
+				sfx.PlaybackSpeed = .9
+				sfx:Play()
+				game:GetService("ContentProvider"):PreloadAsync({sfx})
+				wait(.4)
+				sfx:Destroy()
+			end
+		else
+			local did = false
+			for i,TargetBrick in pairs(AnchorTargets) do
+				if TargetBrick then
+					Anchors[TargetBrick] = {TargetBrick,TargetBrick.CFrame,TargetBrick.Color,TargetBrick.Velocity,TargetBrick.RotVelocity}
+					TargetBrick.Color = Color3.new(0,0,1)
+					did = true
+				end
+			end
+			if did then
+				local sfx = Instance.new("Sound",game.SoundService)
+				sfx.Volume = 1
+				sfx.SoundId = AnchorSound
+				sfx:Play()
+				game:GetService("ContentProvider"):PreloadAsync({sfx})
+				wait(.4)
+				sfx:Destroy()
+			end
+		end
 	end
 end)
 
