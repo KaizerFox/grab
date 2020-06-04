94409c9e81217f87f789bd8131186152ff75dcca
KaizerFox <29178963+KaizerFox@users.noreply.github.com>
2020-06-04 17:10:51 -0400

    Update grabscript.lua

--- grabscript.lua
+++ grabscript.lua
@@ -3,18 +3,21 @@
 --Rewritten because the old script i made in like 2018 was bad lol
 
 --Config
-local ZoomStudsPerSecond = 30 --Controls the speed that the distance changes when pressing + or - when dragging a physics object
-local IgnoreCharacter = false
+local ZoomStudsPerSecond = 60 --Controls the speed that the distance changes when pressing + or - when dragging a physics object
+local RotStudsPerSecond = 16
+local IgnoreCharacter = true
 local MaxParts = 20
-local Disable_Collision = true
+local Disable_Collision = false
 local Range = 4
 --Script
 local TargetBricks = {}
 local TargetPos
+local TargetRot = CFrame.new(0,0,0)
 local Distance = 0
 local Transparencys = {}
 local CanCollides = {}
 local CurZoom = 0
+local CurRot = Vector3.new(0,0,0)
 
 local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
 local UIS = game:GetService("UserInputService")
@@ -23,17 +26,29 @@ function Lerp(PointA,PointB,Alpha)
 	return PointA * (1-Alpha) + PointB * Alpha
 end
 
+function GetCFrameAngles(CF) --GetCFrameAngles(CFrame)
+	local Temp = Instance.new("Part")
+	Temp.CFrame = CF
+	local Response = Temp.Orientation
+	Temp:Destroy()
+	return Response
+end
+
 function UpdateVelocity(FrameTime)
 	for i,TargetBrick in pairs(TargetBricks) do
 		if (not TargetBrick) or (TargetBrick and not TargetPos) then
 			return
 		end
-		TargetBrick.Velocity = (TargetPos - TargetBrick.CFrame.Position)*workspace:GetRealPhysicsFPS()
+		TargetBrick.Velocity = (TargetPos - TargetBrick.CFrame.Position) * 60
+		
+		TargetBrick.CFrame = TargetBrick.CFrame:Lerp(TargetRot - TargetRot.Position + TargetPos,FrameTime*16)
 	end
 end
 
-function UpdateTargetPos(FrameTime)
+function UpdateTarget(FrameTime)
 	CurZoom = math.clamp(CurZoom,-1,1)
+	CurRot = Vector3.new(math.clamp(CurRot.X,-1,1),math.clamp(CurRot.Y,-1,1),math.clamp(CurRot.Z,-1,1))
+	TargetRot = TargetRot * CFrame.Angles((CurRot.X*FrameTime)*RotStudsPerSecond,(CurRot.Y*FrameTime)*RotStudsPerSecond,(CurRot.Z*FrameTime)*RotStudsPerSecond)
 	Distance = Distance + ((CurZoom*FrameTime)*ZoomStudsPerSecond)
 	TargetPos = ((Mouse.Hit.Position - workspace.CurrentCamera.CFrame.Position).Unit*Distance) + workspace.CurrentCamera.CFrame.Position
 end
@@ -99,6 +114,7 @@ Mouse.Button1Down:Connect(function()
 			local Result = HandleBrick(Part)
 			if Result then
 				table.insert(TargetBricks,#TargetBricks+1,Result)
+				TargetRot = Result.CFrame
 			end
 		end
 	end
@@ -120,8 +136,24 @@ UIS.InputBegan:Connect(function(InputObject)
 	if InputObject.KeyCode == Enum.KeyCode.Equals then
 		CurZoom = CurZoom + 1
 	end
-		if InputObject.KeyCode == Enum.KeyCode.Minus then
+	if InputObject.KeyCode == Enum.KeyCode.Minus then
 		CurZoom = CurZoom - 1
+	end
+	if InputObject.KeyCode == Enum.KeyCode.T then
+		--RotForward
+		CurRot = CurRot + Vector3.new(1,0,0)
+	end
+	if InputObject.KeyCode == Enum.KeyCode.F then
+		--RotLeft
+		CurRot = CurRot + Vector3.new(0,0,1)
+	end
+	if InputObject.KeyCode == Enum.KeyCode.G then
+		--RotBackwards
+		CurRot = CurRot + Vector3.new(-1,0,0)
+	end
+	if InputObject.KeyCode == Enum.KeyCode.H then
+		--RotRight
+		CurRot = CurRot + Vector3.new(0,0,-1)
 	end
 end)
 
@@ -129,13 +161,29 @@ UIS.InputEnded:Connect(function(InputObject)
 	if InputObject.KeyCode == Enum.KeyCode.Equals then
 		CurZoom = CurZoom - 1
 	end
-		if InputObject.KeyCode == Enum.KeyCode.Minus then
+	if InputObject.KeyCode == Enum.KeyCode.Minus then
 		CurZoom = CurZoom + 1
+	end
+	if InputObject.KeyCode == Enum.KeyCode.T then
+		--RotForward
+		CurRot = CurRot + Vector3.new(-1,0,0)
+	end
+	if InputObject.KeyCode == Enum.KeyCode.F then
+		--RotLeft
+		CurRot = CurRot + Vector3.new(0,0,-1)
+	end
+	if InputObject.KeyCode == Enum.KeyCode.G then
+		--RotBackwards
+		CurRot = CurRot + Vector3.new(1,0,0)
+	end
+	if InputObject.KeyCode == Enum.KeyCode.H then
+		--RotRight
+		CurRot = CurRot + Vector3.new(0,0,1)
 	end
 end)
 
 function InitStep(FrameTime)
-	UpdateTargetPos(FrameTime)
+	UpdateTarget(FrameTime)
 	TakeNetworkOwnership(FrameTime)
 	UpdateVelocity(FrameTime)
 end
